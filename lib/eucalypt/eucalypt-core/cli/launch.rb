6e813a258e6a960a29b29d2a7af2414214ee1867
Edwin Onuonga <edwinonuonga@gmail.com>
2018-08-21 20:19:37 +0400

    Add rack environment mapping (aliases)

--- lib/eucalypt/eucalypt-core/cli/launch.rb
+++ lib/eucalypt/eucalypt-core/cli/launch.rb
@@ -10,7 +10,7 @@ module Eucalypt
     def launch(env = ENV['RACK_ENV']||'development')
       directory = File.expand_path('.')
       if Eucalypt.app? directory
-        unless %w[production development test].include? env
+        unless %w[p production d development t test].include? env
           Out.error "Invalid Rack environment #{env.colorize(:bold)}"
           return
         end
@@ -22,11 +22,22 @@ module Eucalypt
           cmd.gsub!('rerun', 'rerun -q') if options[:quiet]
         end
 
+        env = map_env env
+
         puts "Running command: #{cmd.colorize(:bold)}"
         puts "Rack environment: #{env.colorize(:bold)}"
         exec "env RACK_ENV=#{env} #{cmd}"
       else
         Eucalypt::Error.wrong_directory
+      end
+    end
+
+    def map_env(env)
+      case env
+      when ?p then 'production'
+      when ?d then 'development'
+      when ?t then 'test'
+      else env
       end
     end
   end
