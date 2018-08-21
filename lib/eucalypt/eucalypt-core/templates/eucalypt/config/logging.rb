85afa77b4c8589ae151c8f49583be17f952881b4
Edwin Onuonga <edwinonuonga@gmail.com>
2018-08-21 20:11:29 +0400

    Change log directory structure

--- lib/eucalypt/eucalypt-core/templates/eucalypt/config/logging.rb
+++ lib/eucalypt/eucalypt-core/templates/eucalypt/config/logging.rb
@@ -2,17 +2,19 @@ class ApplicationController < Sinatra::Base
   set :logger, Lumberjack::Logger.new
 
   configure :production do
+    require 'fileutils'
     use Rack::CommonLogger, $stdout
 
-    log_name = Time.now.strftime("%Y-%m-%dT%H-%M-%S_%z").sub(/_\+/,'_p').sub(/_\-/,'_m')
+    log_path = Eucalypt.path 'log', Time.now.strftime("%Y-%m-%dT%H-%M-%S_%z").sub(/_\+/, ?p).sub(/_\-/, ?m)
+    FileUtils.mkdir_p log_path
 
     # STDERR logger
-    stderr_log = File.new Eucalypt.path('log', "#{log_name}.stderr.log"), 'a+'
+    stderr_log = File.new File.join(log_path, 'stderr.log'), 'a+'
     $stderr.reopen stderr_log
     $stderr.sync = true
 
     # STDOUT logger
-    stdout_log = File.new Eucalypt.path('log', "#{log_name}.stdout.log"), 'a+'
+    stdout_log = File.new File.join(log_path, 'stdout.log'), 'a+'
     $stdout.reopen stdout_log
     $stdout.sync = true
   end
