c980303035773a4737839a16972013c05267a4ec
Edwin Onuonga <edwinonuonga@gmail.com>
2018-08-27 16:57:12 +0400

    Change default spec example titles

--- lib/eucalypt/eucalypt-core/templates/eucalypt/spec/helpers/application_helper_spec.rb
+++ lib/eucalypt/eucalypt-core/templates/eucalypt/spec/helpers/application_helper_spec.rb
@@ -3,7 +3,7 @@ require_relative '../spec_helper'
 describe ApplicationHelper do
   include ApplicationHelper
 
-  it "should expect true to be true" do
+  it "should expect true to be false" do
     expect(true).to be false
   end
 end
