4ac0614446c94f542206d3a1e7f6bc20471be587
Edwin Onuonga <edwinonuonga@gmail.com>
2018-10-09 02:33:30 +0100

    Spacing

--- lib/eucalypt.rb
+++ lib/eucalypt.rb
@@ -5,13 +5,11 @@ require 'eucalypt/generate/namespaces/generate/cli/generate'
 require 'eucalypt/destroy/namespaces/destroy/cli/destroy'
 require 'eucalypt/security/namespaces/security/cli/security'
 require 'eucalypt/migration/namespaces/migration/cli/migration'
-
 require 'eucalypt/static'
 require 'eucalypt/controller'
 require 'eucalypt/app'
 require 'eucalypt/root'
 require 'eucalypt/list'
-
 require 'eucalypt/core/helpers/manifest'
 require 'eucalypt/core/helpers/partial'
 
