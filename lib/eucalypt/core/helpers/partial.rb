706c2df82bd9714784bc490bdd0ca584199bd623
Edwin Onuonga <edwinonuonga@gmail.com>
2018-12-04 04:56:21 +0000

    Fix partial returned object

--- lib/eucalypt/core/helpers/partial.rb
+++ lib/eucalypt/core/helpers/partial.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   helpers do
     def partial(template, **locals)
       path = template.to_s.start_with?('partials/') ? template : "partials/#{template}"
-      erb template.to_sym, layout: false, locals: locals
+      erb path.to_sym, layout: false, locals: locals
     end
   end
 end
