183a193fc686d2ef6a8688b2d0aed47ec87cb7ab
Edwin Onuonga <edwinonuonga@gmail.com>
2018-12-04 04:11:58 +0000

    Prefix eucalypt test command with bundle exec

--- lib/eucalypt/core/cli/test.rb
+++ lib/eucalypt/core/cli/test.rb
@@ -1,13 +1,18 @@
 require_relative '__base__'
 module Eucalypt
   class CLI < Thor
+    using String::Builder
     using Colorize
     option :summarized, type: :boolean, default: false, aliases: '-s', desc: 'rspec -fd spec'
     desc "test", "Run all application tests".colorize(:grey)
     def test
       directory = File.expand_path('.')
       if Eucalypt.app? directory
-        exec (options[:summarized] ? "rspec -fd spec" : "rspec spec")
+        cmd = 'bundle exec rspec '.build do |s|
+          s << '-fd ' if options[:summarized]
+          s << 'spec'
+        end
+        exec cmd
       else
         Eucalypt::Error.wrong_directory
       end
