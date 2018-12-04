d26dad8b1eea35a5f9ce18706646b676e5881a73
Edwin Onuonga <edwinonuonga@gmail.com>
2018-12-04 04:21:50 +0000

    Fix partial helper condition

--- lib/eucalypt/core/helpers/partial.rb
+++ lib/eucalypt/core/helpers/partial.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 class ApplicationController < Sinatra::Base
   helpers do
     def partial(template, **locals)
-      path = template.to_s.start_with? 'partials/' ? template : "partials/#{template}"
+      path = template.to_s.start_with?('partials/') ? template : "partials/#{template}"
       erb template.to_sym, layout: false, locals: locals
     end
   end
