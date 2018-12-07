0f71ee9d971c71b1fa823f409e072934664acff4
Edwin Onuonga <edwinonuonga@gmail.com>
2018-12-07 05:02:49 +0000

    Use clearer variable names in config/logging.rb

--- lib/eucalypt/core/templates/eucalypt/config/logging.rb
+++ lib/eucalypt/core/templates/eucalypt/config/logging.rb
@@ -3,22 +3,23 @@ class ApplicationController < Sinatra::Base
   set :logger, Lumberjack::Logger.new
   helpers { def logger() settings.logger end }
 
-  #= General logging =#
-  %i[production test].each do |e|
-    configure e do
+  # General logging
+  %i[production test].each do |app_env|
+    configure app_env do
       use Rack::CommonLogger, $stdout
 
-      log_path = Eucalypt.path 'log', Time.now.strftime("%Y-%m-%dT%H-%M-%S_%z").sub(/_\+/, ?p).sub(/_\-/, ?m)
+      time = Time.now.strftime("%Y-%m-%dT%H-%M-%S_%z")
+      log_path = Eucalypt.path 'log', time.sub(/_\+/, ?p).sub(/_\-/, ?m)
       FileUtils.mkdir_p log_path
 
-      $stderr.reopen File.new(File.join(log_path, "#{e}.stderr.log"), 'a+')
+      $stderr.reopen File.new(File.join(log_path, "#{app_env}.stderr.log"), 'a+')
       $stderr.sync = true
-      $stdout.reopen File.new(File.join(log_path, "#{e}.stdout.log"), 'a+')
+      $stdout.reopen File.new(File.join(log_path, "#{app_env}.stdout.log"), 'a+')
       $stdout.sync = true
     end
   end
 
-  #= ActiveRecord logging =#
+  # ActiveRecord logging
   ActiveRecord::Base.logger = Logger.new STDOUT
   ActiveRecord::Migration.verbose = true # Set to false for quieter logging
 end
