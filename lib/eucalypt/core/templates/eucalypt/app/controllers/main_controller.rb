83b4a76a5782fe8855525bdd3cb17d675e1aa8ea
Edwin Onuonga <edwinonuonga@gmail.com>
2019-01-03 03:51:36 +0400

    Remove maintenance routes + comment index route

--- lib/eucalypt/core/templates/eucalypt/app/controllers/main_controller.rb
+++ lib/eucalypt/core/templates/eucalypt/app/controllers/main_controller.rb
@@ -1,13 +1,8 @@
 class MainController < ApplicationController
   helpers MainHelper if defined? MainHelper
-
-  static '/maintenance.html', aliases: %w[/maintenance]
-
-  maintenance do
-    redirect '/maintenance'
-  end
-
-  get '/' do
-    erb :index
-  end
+  # Add your main application routes here, like:
+  #
+  # get '/' do
+  #  erb :index
+  # end
 end
