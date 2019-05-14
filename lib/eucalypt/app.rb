6bce48bd45f1d3232fc11a199aac233e0ddf3d3d
Edwin Onuonga <edwinonuonga@gmail.com>
2019-05-14 21:30:57 +0100

    Add environment options for 'console' command

--- lib/eucalypt/app.rb
+++ lib/eucalypt/app.rb
@@ -1,6 +1,11 @@
 module Eucalypt
   APP_FILE = 'Gumfile'.freeze
+
   def self.app?(directory)
     File.exist? File.join(directory, APP_FILE)
+  end
+
+  def self.console?
+    !!ENV['CONSOLE']
   end
 end
--- lib/eucalypt/core/cli/console.rb
+++ lib/eucalypt/core/cli/console.rb
@@ -2,13 +2,34 @@ require_relative '__base__'
 module Eucalypt
   class CLI < Thor
     using Colorize
-    desc "console", "Interactive console with all files loaded".colorize(:grey)
-    def console
+    desc "console [ENV]", "Interactive console with all files loaded".colorize(:grey)
+    def console(env = ENV['APP_ENV']||'development')
       directory = File.expand_path('.')
       if Eucalypt.app? directory
-        exec 'bundle exec irb -r ./app.rb'
+        unless %w[p production d development t test].include? env
+          Out.error "Invalid Rack environment #{env.colorize(:bold)}"
+          return
+        end
+
+        env = map_env env
+        cmd = "CONSOLE=true bundle exec irb -r ./app.rb"
+
+        puts "Running command: #{cmd.colorize(:bold)}"
+        puts "Rack environment: #{env.colorize(:bold)}"
+        exec "env APP_ENV=#{env} #{cmd}"
       else
         Eucalypt::Error.wrong_directory
+      end
+    end
+
+    no_tasks do
+      def map_env(env)
+        case env
+        when ?p then 'production'
+        when ?d then 'development'
+        when ?t then 'test'
+        else env
+        end
       end
     end
   end
